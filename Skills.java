421a36c147dfb42e070f5769a13e94801a7a16f0
VernBuck <backvernon@gmail.com>
2019-11-12 13:01:16 -0500

    Add files via upload

---
+++ Skills.java
@@ -0,0 +1,13 @@
+public class Skills {
+    boolean unlocked = true;
+    private String ability;
+    public void profTitle() {
+        System.out.println("Your Profession");
+    }
+    public String getAbility() {
+        return ability;
+    }
+    public void setAbility(String ability) {
+        this.ability = ability;
+    }
+}
